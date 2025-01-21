3f57167a31ef41893243c25b85c90dc6e282a550
Julius Caezar <julius.caezar25@gmail.com>
2025-01-21 10:32:29 -0300

    feat: criando o a navegação do News

--- src/components/nav-secondary.tsx
+++ src/components/nav-secondary.tsx
@@ -1,39 +1,38 @@
-import * as React from "react"
-import { type LucideIcon } from "lucide-react"
+import * as React from 'react'
 
-import {
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar"
+import { SidebarGroup, SidebarGroupContent } from '@/components/ui/sidebar'
+import { News, NewsArticle } from './ui/sidebar-news'
+
+const DEMO_ARTICLES: NewsArticle[] = [
+  {
+    href: 'https://dub.co/changelog/regions-support',
+    title: 'Regions support in analytics',
+    summary: 'You can now filter your analytics by regions',
+    image: '/bg-gradient.jpeg'
+  },
+  {
+    href: 'https://dub.co/blog/soc2',
+    title: 'Dub is now SOC 2 Type II Compliant',
+    summary:
+      "We're excited to announce that Dub has successfully completed a SOC 2 Type II audit to further demonstrate our commitment to security.",
+    image: '/bg-gradient.jpeg'
+  },
+  {
+    href: 'https://dub.co/changelog/utm-templates',
+    title: 'UTM Templates',
+    summary:
+      'You can now create UTM templates to streamline UTM campaign management across your team.',
+    image: '/bg-gradient.jpeg'
+  }
+]
 
 export function NavSecondary({
-  items,
   ...props
-}: {
-  items: {
-    title: string
-    url: string
-    icon: LucideIcon
-  }[]
-} & React.ComponentPropsWithoutRef<typeof SidebarGroup>) {
+}: React.ComponentPropsWithoutRef<typeof SidebarGroup>) {
   return (
     <SidebarGroup {...props}>
       <SidebarGroupContent>
-        <SidebarMenu>
-          {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton asChild size="sm">
-                <a href={item.url}>
-                  <item.icon />
-                  <span>{item.title}</span>
-                </a>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          ))}
-        </SidebarMenu>
+        <News articles={DEMO_ARTICLES} />
       </SidebarGroupContent>
     </SidebarGroup>
   )
