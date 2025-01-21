75fbce2753da4032fe84e957f1dca747221a62e6
Julius Caezar <julius.caezar25@gmail.com>
2025-01-21 10:20:29 -0300

    feat: adicionando estilos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config = {
   prefix: "",
   theme: {
   	container: {
-  		center: 'true',
+  		center: true,
   		padding: '2rem',
   		screens: {
   			'2xl': '1400px'
@@ -89,6 +89,10 @@ const config = {
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out'
+  		},
+  		transitionProperty: {
+  			height: 'height',
+  			spacing: 'margin, padding'
   		}
   	}
   },
