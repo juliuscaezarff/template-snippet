f210ed2e018960b8e42b2171257041bfe8e1e583
Julius Caezar <julius.caezar25@gmail.com>
2025-01-20 22:14:07 -0300

    feat: refazendo o layout da página e adicionando o componente de notification

--- src/app/app/(main)/page.tsx
+++ src/app/app/(main)/page.tsx
@@ -4,48 +4,63 @@ import {
   BreadcrumbLink,
   BreadcrumbList,
   BreadcrumbPage,
-  BreadcrumbSeparator
 } from '@/components/ui/breadcrumb'
 import { Separator } from '@/components/ui/separator'
 import { SidebarTrigger } from '@/components/ui/sidebar'
+import { getUserTodos } from './actions'
+import { TodoDataTable } from './_components/todo-data-table'
+import { Button } from '@/components/ui/button'
+import { TodoUpsertSheet } from './_components/todo-upsert-sheet'
+import { PlusIcon } from 'lucide-react'
+import { Notifications } from '@/components/ui/notification'
 
 export default async function Page() {
+  const todos = await getUserTodos()
+
   return (
     <div className="relative flex h-full w-full flex-col">
       <header className="flex h-16 shrink-0 items-center gap-2 z-10">
-        <div className="flex items-center gap-2 px-4">
-          <SidebarTrigger className="-ml-1" />
-          <Separator orientation="vertical" className="mr-2 h-4" />
-          <Breadcrumb>
-            <BreadcrumbList>
-              <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Agendamentos
-                </BreadcrumbLink>
-              </BreadcrumbItem>
-              <BreadcrumbSeparator className="hidden md:block" />
-              <BreadcrumbItem>
-                <BreadcrumbPage>Seus Agendamentos</BreadcrumbPage>
-              </BreadcrumbItem>
-            </BreadcrumbList>
-          </Breadcrumb>
+        <div className="flex w-full items-center justify-between px-4">
+          <div className="flex items-center gap-2">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  <BreadcrumbPage className="text-muted-foreground">
+                    Agendamentos
+                  </BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+          <div className="flex items-center gap-2">
+         
+            <Notifications />
+          </div>
         </div>
       </header>
 
-      {/* Background dots pattern */}
-      <div className="absolute inset-0 z-0">
-        <div className="h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:24px_24px] dark:bg-[radial-gradient(#374151_1px,transparent_1px)] dark:opacity-40" />
-      </div>
+      <main className="flex-1 overflow-auto">
+        <div className="container mx-auto max-w-6xl p-6">
+          <div className="flex items-center justify-between mb-4">
+            <div>
+              <h1 className="text-3xl font-bold tracking-tight">Services</h1>
+              <p className="text-muted-foreground mt-1">
+                Manage each application that publish or consume events.
+              </p>
+            </div>
+            <TodoUpsertSheet>
+              <Button size="sm">
+                <PlusIcon className="w-4 h-4" />
+                Add todo
+              </Button>
+            </TodoUpsertSheet>
+          </div>
 
-      {/* Main content */}
-      <main className="relative z-1 flex-1 p-4 pt-0">
-        <div className="grid auto-rows-min gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          <div className="aspect-video rounded-xl bg-muted/50 border" />
-          <div className="aspect-video rounded-xl bg-muted/50 border" />
-          <div className="aspect-video rounded-xl bg-muted/50 border" />
-          <div className="aspect-video rounded-xl bg-muted/50 border" />
-          <div className="aspect-video rounded-xl bg-muted/50 border" />
-          <div className="aspect-video rounded-xl bg-muted/50 border" />
+          <Separator className="my-6" />
+
+          <TodoDataTable data={todos} />
         </div>
       </main>
     </div>
