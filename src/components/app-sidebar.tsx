8c01c03be29b0aa0351fde7ba4600eff458d8851
Julius Caezar <julius.caezar25@gmail.com>
2025-01-23 23:26:06 -0300

    feat: adicionando o collapsible para animação de fechar a sidebar -- adicionando o novo TeamSwitcher

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import * as React from 'react'
-import { Command } from 'lucide-react'
+import { GalleryVerticalEnd, AudioWaveform, Command } from 'lucide-react'
 import { routes } from '@/lib/routes'
 import { NavMain } from '@/components/nav-main'
 import { NavSecondary } from '@/components/nav-secondary'
@@ -11,38 +11,40 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem
 } from '@/components/ui/sidebar'
+import { TeamSwitcher } from './team-switcher'
 
 const data = {
   user: {
     name: 'Julius Caezar',
     email: 'm@example.com',
     avatar: '/avatars/shadcn.jpg'
-  }
+  },
+  teams: [
+    {
+      name: "Acme Inc",
+      logo: GalleryVerticalEnd,
+      plan: "Enterprise",
+    },
+    {
+      name: "Acme Corp.",
+      logo: AudioWaveform,
+      plan: "Startup",
+    },
+    {
+      name: "Evil Corp.",
+      logo: Command,
+      plan: "Free",
+    },
+  ],
+
 }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar variant="inset" {...props}>
+    <Sidebar variant="inset" {...props} collapsible='icon'>
       <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton size="lg" asChild>
-              <div>
-                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <Command className="size-4" />
-                </div>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-semibold">Help Mind</span>
-                  <span className="truncate text-xs">Enterprise</span>
-                </div>
-              </div>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
+        <TeamSwitcher teams={data.teams}/>
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={routes.navMain} />
