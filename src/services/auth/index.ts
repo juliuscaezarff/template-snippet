a391d12f92a44a220be601f95fd3d41da0c78659
Julius Caezar <julius.caezar25@gmail.com>
2025-01-23 23:15:16 -0300

    feat: adicionando o provider do google

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import EmailProvider from 'next-auth/providers/nodemailer'
+import Google from 'next-auth/providers/google'
 
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { prisma } from '../database'
@@ -7,28 +8,32 @@ import { createStripeCustomer } from '../stripe'
 
 export const {
   handlers: { GET, POST },
-  auth,
+  auth
 } = NextAuth({
   pages: {
     signIn: '/auth',
     signOut: '/auth',
     error: '/auth',
     verifyRequest: '/auth',
-    newUser: '/app',
+    newUser: '/app'
   },
   adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
       server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
+      from: process.env.EMAIL_FROM
     }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET
+    })
   ],
   events: {
-    createUser: async (message) => {
+    createUser: async message => {
       await createStripeCustomer({
         name: message.user.name as string,
-        email: message.user.email as string,
+        email: message.user.email as string
       })
-    },
-  },
+    }
+  }
 })
